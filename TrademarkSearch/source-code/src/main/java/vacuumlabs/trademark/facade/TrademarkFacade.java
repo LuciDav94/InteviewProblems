cd97fe81b1b2fafa33313958ffafebc41e8adae8
lucian.davidescu <lucian.davidescu@codemart.ro>
2022-08-16 15:15:06 +0300

    Implement SpringAOP example

--- TrademarkSearch/source-code/src/main/java/vacuumlabs/trademark/facade/TrademarkFacade.java
+++ TrademarkSearch/source-code/src/main/java/vacuumlabs/trademark/facade/TrademarkFacade.java
@@ -2,6 +2,7 @@ package vacuumlabs.trademark.facade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vacuumlabs.trademark.aop.LogExecutionTime;
 import vacuumlabs.trademark.model.Trademark;
 import vacuumlabs.trademark.service.TrademarkRepository;
 
@@ -12,13 +13,13 @@ public class TrademarkFacade {
 
     public TrademarkRepository trademarkRepository;
 
+    @LogExecutionTime
     public Trademark getTradeMark(String name) {
         return trademarkRepository.findByWordMarkSpecification(name);
     }
 
+    @LogExecutionTime
     public List<Trademark> getTradeMarks(String name) {
-        Long name;
-        Strin
         return trademarkRepository.findByWordMarkSpecificationContainingIgnoreCase(name);
     }
 
