1a7f9ae94ae6ea0e050095d4dd9b5834cfdde92a
Lucian Davidescu <lucian.davidescu94@gmail.com>
2023-06-27 20:47:29 +0300

    delete unused

--- SatelliteSearch/satellite-frontend/src/api/middleware.ts
+++
@@ -1,3 +0,0 @@
-import { satelliteApi } from '~/api/satellite';
-
-export const apiMiddleware = [satelliteApi.middleware];
--- SatelliteSearch/satellite-frontend/src/api/reducers.ts
+++
@@ -1,5 +0,0 @@
-import { satelliteApi } from '~/api/satellite';
-
-export const apiReducers = {
-  [satelliteApi.reducerPath]: satelliteApi.reducer,
-};
